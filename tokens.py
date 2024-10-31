58559901151ae0e56ad4ee3a562685266a6bdbdf
Rasheek <sallanrasheek@gmail.com>
2024-11-01 01:47:17 +0530

    added decrement, negation , bitwise tokens'

--- tokens.py
+++ tokens.py
@@ -1,12 +1,15 @@
 TOKEN_SPECIFICATIONS = [
-    ("Int Keyword", r"int\b"),           # Keyword: int
-    ("Void Keyword", r"void\b"),         # Keyword: void
-    ("Return Keyword", r"return\b"),     # Keyword: return
-    ("Identifier", r"[a-zA-Z_]\w*"),     # Valid identifiers
-    ("Constant", r"[0-9]+"),             # Integer constant
-    ("Open parenthesis", r"\("),         # Open parenthesis
-    ("Close parenthesis", r"\)"),        # Close parenthesis
-    ("Open brace", r"\{"),               # Open brace
-    ("Close brace", r"\}"),              # Close brace
-    ("Semicolon", r";"),                 # Semicolon
+    ("Int Keyword", r"int\b"),  # Keyword: int
+    ("Void Keyword", r"void\b"),  # Keyword: void
+    ("Return Keyword", r"return\b"),  # Keyword: return
+    ("Identifier", r"[a-zA-Z_]\w*"),  # Valid identifiers
+    ("Constant", r"[0-9]+"),  # Integer constant
+    ("Open parenthesis", r"\("),  # Open parenthesis
+    ("Close parenthesis", r"\)"),  # Close parenthesis
+    ("Open brace", r"\{"),  # Open brace
+    ("Close brace", r"\}"),  # Close brace
+    ("Semicolon", r";"),  # Semicolon
+    ("Bitwise Complement", r"~"),
+    ("Negation Operator", r"-"),
+    ("Decrement Operator", r"--"),
 ]
